22eca695ee0c3e6f3c53a403f4fca1df582c91ce
SaiyobDdos <saiyob.ddos@gmail.com>
2024-09-05 16:00:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7505040379:AAGvmIec4z4jBVZ1mI5V92Wa4f9KzTfDUz4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@black_devilyt")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("969452944")
 
 
 
